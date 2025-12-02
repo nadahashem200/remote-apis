b7278fdf645073a4cb66ddaa02c271774430ff54
Nada Hashem <nadahashem200@gmail.com>
2025-12-02 17:04:15 +0200

    Define all variables/DOM constants, implement function placeholders, and set up app initialization.

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
+// Counters
 let dogsViewed = 0;
 let factsLearned = 0;
 
 
+// Display Elements
+const dogImage = document.getElementById("dogImage");
+const breedName = document.getElementById("breedName");
+const imageLoading = document.getElementById("imageLoading");
+const dogFact = document.getElementById("dogFact");
+const factLoading = document.getElementById("factLoading");
+
+// Stats Elements
+const dogsViewedElement = document.getElementById("dogsViewed");
+const factsLearnedElement = document.getElementById("factsLearned");
+
+// Buttons 
+const newDogBtn = document.getElementById("newDogBtn");
+const newImageBtn = document.getElementById("newImageBtn");
+const newFactBtn = document.getElementById("newFactBtn");
+
+
+
+function loadStats() {} 
+function setupButtons() {} 
+function loadBothDogAndFact() {} 
+function loadDogImage() {}
+function loadDogFact() {}
+
+
+
 window.addEventListener('DOMContentLoaded', function() {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    loadStats();
+    setupButtons();
+    loadBothDogAndFact();
 });
